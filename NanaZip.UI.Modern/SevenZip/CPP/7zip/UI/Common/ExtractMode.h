1ed6d0172ca9648d61d65ad1cc6c806953629c85
Tu Dinh <contact@tudinh.xyz>
2025-05-08 15:53:00 +0200

    Set default MOTW mode to Office files.
    Affects also NanaZip.Core (and therefore SFX archives).

--- NanaZip.UI.Modern/SevenZip/CPP/7zip/UI/Common/ExtractMode.h
+++ NanaZip.UI.Modern/SevenZip/CPP/7zip/UI/Common/ExtractMode.h
@@ -37,6 +37,10 @@ namespace NZoneIdMode
     kAll,
     kOffice
   };
+
+  // **************** NanaZip Modification Start ****************
+static inline const EEnum Default = kOffice;
+  // **************** NanaZip Modification End ****************
 }
 
 }
